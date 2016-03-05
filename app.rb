39921fb1d7e96e7923bff5d3cfaaec59b6e30c20
Kunal Bhat <kunalbhat@gmail.com>
2016-03-05 13:49:52 -0600

    Method for sending email notification via mailgun

--- app.rb
+++ app.rb
@@ -6,6 +6,17 @@ Bundler.require
 require_relative 'config/datamapper'
 require_relative 'models/post'
 
+def send_notification
+  mg_client = Mailgun::Client.new "#{ENV['MAILGUN_API_KEY_SANDBOX']}"
+
+  message_params = {:from    => "Mailgun Sandbox <#{ENV['MAILGUN_FROM']}>",
+                    :to      => "Kunal Bhat <#{ENV['MAILGUN_TO']}>",
+                    :subject => 'NEW FREDDIE AVAILABLE!!!',
+                    :text    => 'Go get it at mailchimp.com/replyall !!!'}
+
+  mg_client.send_message "#{ENV['MAILGUN_FROM'].split('@')[1]}", message_params
+end
+
 def update_freddie(new_freddie)
   post_to_update = Post.last
   post_to_update.update(:title => new_freddie)
