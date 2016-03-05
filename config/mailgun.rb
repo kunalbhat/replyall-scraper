b1cddc769896d2f267b10c4181cb5686a2fa72a6
Kunal Bhat <kunalbhat@gmail.com>
2016-03-05 15:42:43 -0600

    Missing closing brace in mailgun setup

--- config/mailgun.rb
+++ config/mailgun.rb
@@ -6,4 +6,4 @@ email_body    = "Go get it at mailchimp.com/replyall"
 @message_params = {:from   => "#{ENV['MAILGUN_FROM_NAME']} <#{ENV['MAILGUN_FROM']}>",
                   :to      => "#{ENV['MAILGUN_TO_NAME']} <#{ENV['MAILGUN_TO']}>",
                   :subject => "#{email_subject}",
-                  :text    => "#{email_body}"
+                  :text    => "#{email_body}"}
